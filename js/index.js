bba11214570809dc01231cb05928e1f432ebfc27
Cecilia Limay <mcecilia.limay@gmail.com>
2016-06-08 13:30:52 -0500

    portafolio actualizado al 8 de Junio

--- js/index.js
+++ js/index.js
@@ -211,16 +211,73 @@ setTimeout(function() {
 },5200);
 
 
+/*SLIDER MARCAS*/
+
+$(function () {
+
+    var count = $("#slider > img").length
+    var slider = 1
+    var speed = 2200
+    var fadeSpeed = 500
+    var loop
+
+    start()
+
+    $("#1").fadeIn(fadeSpeed);
+    $("#1a").addClass("selected");
+    $('.right').click(right);
+    $('.left').click(left);
+    $('#slider').hover(stop, start);
+
+    function start() {
+        loop = setInterval(next, speed)
+    }
+
+    function stop() {
+        clearInterval(loop)
+    }
+
+    function right() {
+        stop()
+        next()
+        start()
+        return false
+    }
+
+    function left() {
+        stop()
+        prev()
+        start()
+        return false
+    }
+
+    function prev() {
+        $("#" + slider + "a").removeClass("selected")
+        slider--
+        if (slider < 1) {
+            slider = count
+        }
+
+        $("#slider > img").fadeOut(fadeSpeed)
+
+        $("#" + slider).fadeIn(fadeSpeed)
+        $("#" + slider + "a").addClass("selected")
+    }
+
+    function next() {
+        $("#" + slider + "a").removeClass("selected")
+        slider++
+        if (slider > count) {
+            slider = 1
+        }
+
+        $("#slider > img").fadeOut(fadeSpeed)
+        $("#" + slider).fadeIn(fadeSpeed)
+        $("#" + slider + "a").addClass("selected")
+    }
 
 
-
-
-
-
-
-
-
-
+});
 
 
 
--- js/main.js
+++ js/main.js
@@ -2,88 +2,38 @@ function skill() {
         location.reload(true); 
     } 
 
-/*MENU*/
-$('.toggle').on('click', function() {
-  $('.menu').toggleClass('expanded');  
-  $('span').toggleClass('hidden');  
-  $('.contenedor , .toggle').toggleClass('close');  
+
+
+
+var strings = {
+  first: [
+    'Analisémosla',
+    'planifiquémosla',
+    'Codifiquémosla',
+    'hagámosla realidad'
+  ],
+  second: [
+    'hackathons',
+    'events'
+  ],
+  third: ['stuff']
+}
+
+$('.txt').typed({
+  strings: strings.first,
+  typeSpeed: 100,
+  backDelay: 1500,
+  callback: function () {
+    $('.typed-cursor').fadeOut()
+  }
+})
+
+
+
+
+$('.link').click(function(e){
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
 });
-
-
-/*EFECTO MÁQUINA DE ESCRIBIR*/
-function maquina(contenedor,texto,intervalo){
-   // Calculamos la longitud del texto
-   longitud = texto.length;
-   // Obtenemos referencia del div donde se va a alojar el texto.
-   cnt = document.getElementById(contenedor);
-   var i=0;
-   // Creamos el timer
-   timer = setInterval(function(){
-      // Vamos añadiendo letra por letra y la _ al final.
-      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "|";
-      // Si hemos llegado al final del texto..
-      if(i >= longitud){
-         // Salimos del Timer y quitamos la barra baja (_)
-         clearInterval(timer);
-         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
-         return true;
-      } else {
-         // En caso contrario.. seguimos
-         i++;
-      }},intervalo);
-};
-
-var texto = "¿Tienes una idea en mente? Codifiquémosla.";
-// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
-maquina("maquinas",texto,100);
-
-/*SCROLL*/
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-jQuery(function ($) {
-
-    $('.centered').each(function (e) {
-        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
-    });
-
-    $(window).resize(function () {
-        $('.centered').each(function (e) {
-            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
-        });
-    });
-
-
-    //contact form
-    var form = $('.contact-form');
-    form.submit(function () {
-        $this = $(this);
-        $.post($(this).attr('action'), function (data) {
-            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-        }, 'json');
-        return false;
-    });
-
-    //goto top
-    $('.gototop').click(function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $("body").offset().top
-        }, 500);
-    });
-});
-
 
 
